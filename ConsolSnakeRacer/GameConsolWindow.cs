8bf139bc3748c2f7b00b7cf5f25a468970b92070
koteR <42820377+vbaaccess@users.noreply.github.com>
2021-09-02 00:55:49 +0200

    Program glowny + menu

---
+++ ConsolSnakeRacer/GameConsolWindow.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsolSnakeRacer
+{
+    public static class GameConsolWindow
+    {
+
+        public static void Size(int consolSize_X, int consolSize_y)
+        {
+            Console.SetWindowSize(consolSize_X, consolSize_y);
+            Console.BufferHeight = consolSize_X;
+            Console.BufferWidth = consolSize_y;
+        }
+
+        public static void Center()
+        {
+            var consoleWnd = System.Diagnostics.Process.GetCurrentProcess().MainWindowHandle;
+            Imports.SetWindowPos(consoleWnd, 0, 300, 300, 0, 0, Imports.SWP_NOSIZE | Imports.SWP_NOZORDER);
+            //TO DO - optymaliz center consol window
+        }
+    }
+
+    static class Imports
+    {
+        public static IntPtr HWND_BOTTOM = (IntPtr)1;
+        // public static IntPtr HWND_NOTOPMOST = (IntPtr)-2;
+        public static IntPtr HWND_TOP = (IntPtr)0;
+        // public static IntPtr HWND_TOPMOST = (IntPtr)-1;
+
+        public static uint SWP_NOSIZE = 1;
+        public static uint SWP_NOZORDER = 4;
+
+        [DllImport("user32.dll", EntryPoint = "SetWindowPos")]
+        public static extern IntPtr SetWindowPos(IntPtr hWnd, int hWndInsertAfter, int x, int Y, int cx, int cy, uint wFlags);
+    }
+
+}
---
+++ ConsolSnakeRacer/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsolSnakeRacer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int gameBoardSize = 54;                 // ustawienia rozmiaru obszaru gry
+
+            //ustawienia okna konsoli:
+            Console.CursorVisible = false;          // wylaczenie migajacego kursora
+            GameConsolWindow.Size(gameBoardSize     // - ustawienia rozmiaru
+                            , gameBoardSize);
+            GameConsolWindow.Center();              // - wysrodkowanie okna
+
+            // wstepne menu gry (nazie bez opcji)
+            string s;
+            Console.Clear();
+            Console.Title = "CONSOL GAME - SNAKE RACER 2021 (c)";
+            s = "CONSOL GAME SNAKE RACER";
+            Console.SetCursorPosition((int)gameBoardSize / 2 - ((int)(s.Length) / 2), 1);
+            Console.Write(s);
+
+            s = "PRES KEY TO START GAME!";
+            Console.SetCursorPosition((int)gameBoardSize / 2 - ((int)(s.Length) / 2), gameBoardSize / 2);
+            Console.Write(s);
+            Console.ReadLine();
+            Console.Clear();
+        }
+    }
+}
